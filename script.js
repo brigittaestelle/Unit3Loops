7999a34f0c2a7cc9dc516ec8de85ba1fced095e2
brigittaestelle <beburguess@gmail.com>
2021-06-12 16:17:53 -0400

    LoopsExercises

---
+++ script.js
@@ -0,0 +1,31 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+var i = 10;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+var i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+var i = 10;
+do {
+  console.log(i);
+  i--;
+} while (i >= 1);
